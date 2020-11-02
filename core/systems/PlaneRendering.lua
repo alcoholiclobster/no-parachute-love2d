2b54ed21561f7a84b573e27b24c3bd1ded525051
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2020-11-02 13:47:47 +0300

    Added motion blur

--- core/systems/PlaneRendering.lua
+++ core/systems/PlaneRendering.lua
@@ -9,6 +9,7 @@ local PlaneRendering = Concord.system({
 })
 
 local screenWidth, screenHeight = love.graphics.getWidth(), love.graphics.getHeight()
+local canvas = love.graphics.newCanvas()
 local depthPrecision = 4
 local minZ = -100
 local maxZ = 0
@@ -25,6 +26,31 @@ vec4 effect(vec4 vcolor, Image texture, vec2 texcoord, vec2 pixcoord)
     return vec4(texcolor.rgb, texcolor.a * vcolor.a);
 }
 ]]
+
+local blurShader = love.graphics.newShader[[
+uniform float aspect_ratio = 0.0;
+float blur_radius = 3;
+int blur_steps = 4;
+
+vec4 effect(vec4 vcolor, Image texture, vec2 texcoord, vec2 pixcoord)
+{
+    vec2 pos = texcoord;
+    pos.x *= aspect_ratio;
+
+    vec2 dir = pos - vec2(0.5 * aspect_ratio, 0.5);
+    float blur_amount = clamp(pow(length(dir) + 0.1, 3.5), 0.0, 1.0);
+
+    vec4 color = vec4(0.0);
+
+    dir = normalize(dir) * blur_amount / 128.0 * blur_radius;
+    float count = 0.0;
+    for (int i = 0; i < blur_steps; i++)
+    {
+        color += Texel(texture, texcoord - i * dir);
+        count += 1.0;
+    }
+    return color / count;
+}]]
 
 local function render(e, camera)
     local x, y, scale = renderingUtils.project(e.position.value, camera.position.value, camera.camera.fov)
@@ -80,6 +106,8 @@ function PlaneRendering:init()
     fogColor = self:getWorld().gameManager.levelConfig.fogColor or {0, 0, 0}
     fogColor = {fogColor[1] / 255, fogColor[2] / 255, fogColor[3] / 255, 1}
     planeShader:sendColor("fogcolor", fogColor)
+
+    blurShader:send("aspect_ratio", screenWidth / screenHeight)
 end
 
 function PlaneRendering:draw()
@@ -105,6 +133,7 @@ function PlaneRendering:draw()
         end
     end
 
+    love.graphics.setCanvas(canvas)
     love.graphics.clear(fogColor[1], fogColor[2], fogColor[3], 1)
     love.graphics.setShader(planeShader)
     love.graphics.translate(screenWidth/2, screenHeight/2)
@@ -125,6 +154,19 @@ function PlaneRendering:draw()
     end
 
     love.graphics.setShader()
+    love.graphics.setCanvas()
+
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.origin(1, 1, 1, 1)
+    love.graphics.setShader(blurShader)
+    love.graphics.draw(canvas)
+    love.graphics.setShader()
+end
+
+-- TODO: Move to main.lua
+function love.resize(w, h)
+    blurShader:send("aspect_ratio", w / h)
+    canvas = love.graphics.newCanvas()
 end
 
 return PlaneRendering
