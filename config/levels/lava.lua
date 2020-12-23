2f0b2a92b00ae44003a5e05290c5fb66a90b3597
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2020-12-23 16:38:24 +0300

    lava level wip

--- config/levels/lava.lua
+++ config/levels/lava.lua
@@ -21,10 +21,32 @@ return {
     },
 
     planeTypes = {
-
+        corner_hole = {
+            planes = {{ texture = "levels/lava/corner_hole", }},
+        },
+        corner_hole_big = {
+            planes = {{ texture = "levels/lava/corner_hole_big", }},
+        },
+        middle_hole = {
+            planes = {{ texture = "levels/lava/middle_hole", }},
+        },
     },
 
     planes = {
+        { distance = 100, name = "corner_hole", },
+        { distance = 25, name = "corner_hole", rotation = 90, },
+        { distance = 25, name = "corner_hole", rotation = 180, },
+        { distance = 25, name = "corner_hole", rotation = 270, },
+        { distance = 25, name = "corner_hole_big", rotation = 0, },
+        { distance = 19, name = "corner_hole_big", rotation = 90, },
+        { distance = 19, name = "corner_hole_big", rotation = 180, },
+        { distance = 19, name = "corner_hole_big", rotation = 270, },
+        { distance = 19, name = "corner_hole_big", rotation = 0, },
+        { distance = 19, name = "corner_hole_big", rotation = 90, },
+        { distance = 19, name = "corner_hole_big", rotation = 180, },
+        { distance = 19, name = "corner_hole_big", rotation = 270, },
+        { distance = 35, name = "corner_hole", rotation = 90, },
+        { distance = 25, name = "middle_hole", rotation = 0, },
         { distance = 5000000 },
     },
 }
