4e7270bb9e166c55760a77db1c54b854c306e033
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2021-02-09 00:45:35 +0300

    bones level wip

--- config/levels/meat1.lua
+++ config/levels/meat1.lua
@@ -18,6 +18,17 @@ return {
                 "levels/meat/side_planes/cave1",
                 "levels/meat/side_planes/cave2",
             },
+        },
+        {
+            textures = {
+                "levels/meat/side_planes/cave3",
+                "levels/meat/side_planes/cave4",
+            },
+        },
+        {
+            textures = {
+                "levels/meat/side_planes/empty"
+            },
         }
     },
 
@@ -25,21 +36,59 @@ return {
         wall_hands = {
             planes = {{ texture = "levels/meat/obstacles/wall_hands", }},
         },
+        big_skull = {
+            planes = {
+                { texture = "levels/meat/obstacles/big_skull", },
+                { texture = "levels/meat/obstacles/big_skull_eyes", position = {0, 0, -4}, decorative = true  }
+            },
+        },
         side_hand = {
             planes = {{ texture = "levels/meat/obstacles/side_hand", }},
         },
+        moving_hands = {
+            planes = {
+                { texture = "levels/meat/obstacles/big_hand", rotationSpeed = 0.4, position = {-5, 1, 0}},
+                { texture = "levels/meat/obstacles/big_hand_right", rotationSpeed = -0.6, position = {5, 2, 0}},
+            },
+        },
+        huge_bone = {
+            planes = {
+                { texture = "levels/meat/obstacles/huge_bone_test"}
+            }
+        }
     },
 
     planes = {
-        { distance = 100, name = "wall_hands", },
-        { distance = 5, name = "wall_hands", rotation = 90 },
+        -- { distance = 100, name = "moving_hands", },
+        { distance = 120, name = "wall_hands", rotation = 90 },
         { distance = 5, name = "wall_hands", rotation = 180 },
-        { distance = 5, name = "side_hand", },
-        { distance = 10, name = "side_hand", rotation = 180 },
-        { distance = 100, name = "side_hand", },
-        { distance = 100, name = "side_hand", },
-        { distance = 100, name = "side_hand", },
-        { distance = 100, name = "side_hand", },
+        { distance = 6, name = "wall_hands", rotation = 90 },
+        { distance = 3, name = "wall_hands", rotation = 0 },
+        { distance = 8, name = "wall_hands", rotation = 270 },
+        { distance = 4, name = "wall_hands", rotation = 180 },
+        { distance = 2, name = "wall_hands", rotation = 90 },
+        { distance = 10, name = "wall_hands", rotation = 0 },
+        { distance = 2, name = "wall_hands", rotation = 90 },
+        { distance = 5, name = "wall_hands", rotation = 180 },
+        { distance = 6, name = "wall_hands", rotation = 90 },
+        { distance = 3, name = "wall_hands", rotation = 0 },
+        { distance = 8, name = "wall_hands", rotation = 270 },
+        { distance = 4, name = "wall_hands", rotation = 180 },
+        { distance = 2, name = "wall_hands", rotation = 90 },
+        { distance = 10, name = "wall_hands", rotation = 0 },
+        { distance = 6, name = "wall_hands", rotation = 90 },
+        { distance = 5, name = "wall_hands", rotation = 180, switchSidePlanes = true },
+        { distance = 6, name = "wall_hands", rotation = 90 },
+        { distance = 3, name = "wall_hands", rotation = 0 },
+        { distance = 8, name = "wall_hands", rotation = 270 },
+        { distance = 8, name = "big_skull", rotation = 0 },
+        { distance = 50, name = "side_hand", },
+        { distance = 10, name = "side_hand", rotation = 180, switchSidePlanes = true},
+        { distance = 50, name = "huge_bone", rotation = 0 },
+        { distance = 50, name = "huge_bone", rotation = 90 },
+        { distance = 50, name = "huge_bone", rotation = 180 },
+        { distance = 50, name = "huge_bone", rotation = 270 },
+        { distance = 50, name = "huge_bone", rotation = 0 },
 
         { distance = 5000000 },
     },
