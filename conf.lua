8bcd992d1dc19805d34c0866a779dd965f5076de
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2021-03-09 02:51:06 +0300

    fix windows dpi scaling

--- conf.lua
+++ conf.lua
@@ -1,4 +1,17 @@
+local function fixDpiAwareness()
+    local ffi = require("ffi")
+
+    ffi.cdef([[
+        int SetProcessDpiAwareness(int);
+    ]])
+
+    local shcore = ffi.load("shcore")
+    shcore.SetProcessDpiAwareness(2)
+end
+
 function love.conf(t)
+    pcall(fixDpiAwareness)
+
     t.window.title = "No Parachute"
     t.window.width = 1920
     t.window.height = 1080
