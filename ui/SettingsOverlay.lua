943b9eeeaa246e21bfd4fecbda48d6c23f94ce2c
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2021-03-10 02:22:06 +0300

    remove settings overlay rectangle

--- ui/SettingsOverlay.lua
+++ ui/SettingsOverlay.lua
@@ -17,7 +17,7 @@ end
 
 function SettingsOverlay:draw()
     local screenWidth, screenHeight = love.graphics.getWidth(), love.graphics.getHeight()
-    love.graphics.setColor(0, 0, 0, 0.5)
+    love.graphics.setColor(0, 0, 0, 0.9)
     love.graphics.rectangle("fill", 0, 0, screenWidth, screenHeight)
 
     local overlayWidth = screenWidth * 0.5
@@ -25,8 +25,8 @@ function SettingsOverlay:draw()
     local overlayX = (screenWidth - overlayWidth) / 2
     local overlayY = (screenHeight - overlayHeight) / 2
 
-    love.graphics.setColor(0, 0, 0, 0.9)
-    love.graphics.rectangle("fill", overlayX, overlayY, overlayWidth, overlayHeight + overlayHeight * 0.06)
+    -- love.graphics.setColor(0, 0, 0, 0.9)
+    -- love.graphics.rectangle("fill", overlayX, overlayY, overlayWidth, overlayHeight + overlayHeight * 0.06)
 
     local itemX, itemY = overlayX + overlayWidth * 0.05, overlayY + overlayHeight * 0.05
     local itemWidth, itemHeight = overlayWidth * 0.5 - (itemX - overlayX) * 2, overlayHeight * 0.04
@@ -62,7 +62,7 @@ function SettingsOverlay:draw()
         end
     end
 
-    if widgets.button(lz("btn_settings_close"), overlayX, overlayY + overlayHeight - overlayHeight * 0.05, overlayWidth, screenHeight * 0.05, false, "center") then
+    if widgets.button(lz("btn_settings_close"), overlayX, overlayY + overlayHeight, overlayWidth, screenHeight * 0.05, false, "center") then
         self.isClosed = true
     end
 end
