eae8236cb05d5812ad3e11b42fe1e86e9ec6be2e
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2021-03-19 18:13:21 +0300

    intro wip

--- ui/cutscenes/intro/Scene1.lua
+++ ui/cutscenes/intro/Scene1.lua
@@ -9,7 +9,7 @@ function Scene1:initialize()
     local this = self
     self.thread = scheduler.createThread(function () this:process() end)
 
-    self.line = 0
+    self.planeTexture = assets.texture("cutscenes/intro/plane")
 end
 
 function Scene1:onHide()
@@ -17,14 +17,13 @@ function Scene1:onHide()
 end
 
 function Scene1:process()
-    scheduler.wait(3)
-    -- Ok I think I'm ready
-    self.line = self.line + 1
-    scheduler.wait(4)
-
-    -- DO IT
-    self.line = self.line + 1
-    scheduler.wait(3)
+    scheduler.wait(2)
+    self.planeTexture = assets.texture("cutscenes/intro/plane3")
+    scheduler.wait(1)
+    self.planeTexture = assets.texture("cutscenes/intro/plane2")
+    scheduler.wait(2)
+    self.planeTexture = assets.texture("cutscenes/intro/plane")
+    scheduler.wait(1)
 
     self.cutscene:changeScene("Scene2")
 end
@@ -47,19 +46,7 @@ function Scene1:draw()
 
     local planeX = math.cos(love.timer.getTime() * 1.5) * 3 + 3
     local planeY = math.sin(love.timer.getTime() * 4) * 2
-    love.graphics.draw(assets.texture("cutscenes/intro/plane"), planeX, planeY)
-
-    if self.line == 1 then
-        local popupx1 = math.cos(love.timer.getTime() * 1.5 - 1) * 3 + 3
-        local popupy1 = math.sin(love.timer.getTime() * 4 - 1) * 2
-        love.graphics.draw(assets.texture("cutscenes/intro/line1"), popupx1, popupy1)
-    end
-
-    if self.line == 2 then
-        local popupx2 = math.cos(love.timer.getTime() * 1.5 - 1) * 3 + 3
-        local popupy2 = math.sin(love.timer.getTime() * 4 - 2) * 1 + 2
-        love.graphics.draw(assets.texture("cutscenes/intro/line2"), popupx2, popupy2)
-    end
+    love.graphics.draw(self.planeTexture, planeX, planeY)
 end
 
 return Scene1
