ddd9c8a646c28f6f6f86b62b5c1ba24c73f11d88
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2021-03-21 20:54:13 +0300

    ending1 wip

--- config/levels/ending1.lua
+++ config/levels/ending1.lua
@@ -5,7 +5,7 @@ return {
 
     fallSpeed = 55,
     fogColor = {25, 10, 0},
-    fogDistance = 60,
+    fogDistance = 90,
     playerRotationMode = "sinusoid",
     playerRotationSpeed = -0.3,
     playerRotationChangeSpeed = 0.012,
@@ -33,6 +33,17 @@ return {
         },
         {
             textures = {
+                "levels/meat/side_planes/flesh_wide2",
+            },
+        },
+        {
+            textures = {
+                "levels/meat/side_planes/flesh1",
+                "levels/meat/side_planes/flesh2",
+            },
+        },
+        {
+            textures = {
                 "levels/vents/decorative1",
                 "levels/vents/decorative2",
                 "levels/vents/decorative3",
@@ -41,6 +52,7 @@ return {
     },
 
     planeTypes = {
+        -- stone
         stone_breakable_wall = {
             planes = {{ texture = "levels/meat/obstacles/stone_obstacle_breakable", breakable = true }},
         },
@@ -56,6 +68,7 @@ return {
                 { texture = "levels/meat/obstacles/stone_obstacle5", },
             },
         },
+        -- meat
         meat_half_wall = {
             planes = {
                 { texture = "levels/meat/obstacles/meat_decorative_corner_things2", position = {0, 0, 1}, rotation = 180, decorative = true },
@@ -78,6 +91,57 @@ return {
             },
         },
 
+        -- worm
+        worm1_big = {
+            planes = {
+                { texture = "levels/meat/obstacles/worm1", rotationSpeed = 1 }
+            }
+        },
+        worm1_big_head = {
+            planes = {
+                { texture = "levels/meat/obstacles/worm1_head", rotationSpeed = 1 }
+            }
+        },
+        worm1_wall = {
+            planes = {
+                { texture = "levels/meat/obstacles/worm_wall", position = {0, 0, 0} },
+                { texture = "levels/meat/obstacles/worm_wall", position = {0, 0, -2}, rotation = 90 }
+            }
+        },
+        side_worm_wall = {
+            planes = {
+                { texture = "levels/meat/obstacles/side_worm_wall", position = {0, 0, 0} },
+            }
+        },
+        worm_smoke = {
+            planes = {
+                { texture = "levels/meat/obstacles/worm_smoke", position = {0, 1, 0.1}, rotationSpeed = 0.2, velocity = {1, 0, 0}, decorative = true },
+                { texture = "levels/meat/obstacles/worm_smoke", position = {1, 0, -2.1}, rotationSpeed = -0.1, velocity = {-0.25, 0.5, 0}, decorative = true },
+            }
+        },
+        worm_smoke2 = {
+            planes = {
+                { texture = "levels/meat/obstacles/worm_smoke2", position = {0, 1, 0.1}, rotationSpeed = 0.2, velocity = {1, 0, 0}, decorative = true },
+                { texture = "levels/meat/obstacles/worm_smoke2", position = {1, 0, -2.1}, rotationSpeed = -0.1, velocity = {-0.25, 0.5, 0}, decorative = true },
+            }
+        },
+        worm_smoke_head = {
+            planes = {
+                { texture = "levels/meat/obstacles/worm_smoke", position = {0, 6, 0.1}, rotationSpeed = 0.2, velocity = {0, -6, 0}, moveDelay = 0.5, decorative = true },
+            }
+        },
+        meat_big_hole_to_normal = {
+            planes = {
+                { texture = "levels/meat/obstacles/meat_big_holes_to_normal" },
+                { texture = "levels/meat/side_planes/flesh1", position = {0, 0, -2}, rotation = 90 },
+                { texture = "levels/meat/side_planes/flesh2", position = {0, 0, -4}, rotation = 0 },
+                { texture = "levels/meat/side_planes/flesh1", position = {0, 0, -6}, rotation = 180 },
+                { texture = "levels/meat/obstacles/meat_decorative_corner_things", position = {0, 0, -7}, decorative = true },
+                { texture = "levels/meat/side_planes/flesh2", position = {0, 0, -8}, rotation = 270 },
+            }
+        },
+
+        -- vents
         vents_middle_long_stick = {
             planes = {
                 { texture = "levels/vents/long_stick", position = {0, 0, 0}, rotation = 180 },
@@ -104,9 +168,31 @@ return {
         { distance = 20, name = "meat_half_wall" },
         { distance = 10, name = "meat_long_middle_thing", rotation = 90},
         { distance = 10, name = "meat_half_wall", rotation = 90},
-        { distance = 30, name = "meat_wall" },
 
-        { distance = 30, name = "vents_middle_door", switchSidePlanes = true, },
+        { distance = 30, name = "meat_wall" },
+        { distance = 1, switchSidePlanes = true },
+
+        { distance = 150, name = "worm1_wall"},
+        { distance = 0, name = "worm_smoke", position = {-8, 8, 0 }},
+        { distance = 0, name = "worm_smoke2", position = {10, -10, 0 }},
+        { distance = 1, name = "worm1_big", position = {10, 10, 0}},
+
+        { distance = 40, name = "worm1_wall" },
+        { distance = 0, name = "worm_smoke", position = {9, 9, 0 }},
+        { distance = 0, name = "worm_smoke", position = {-10, -10, 0 }},
+        { distance = 1, name = "worm1_big", position = {-8, 8, 0}, rotation = 180},
+
+        { distance = 30, name = "worm1_wall" },
+        { distance = 0, name = "worm_smoke2", position = {-2, -9.5, 0 }},
+        { distance = 0, name = "worm_smoke", position = {-10, -2, 0 }},
+        { distance = 1, name = "worm1_big", position = {15, 15, 0}, rotation = 180},
+
+        { distance = 40, name = "worm1_wall" },
+        { distance = 5, name = "meat_big_hole_to_normal" },
+        { distance = 1, name = "meat_wall", switchSidePlanes = true, },
+        { distance = 10, tunnelShape = { direction = {0, 0}, rotationSpeed = 0 }, },
+
+        { distance = 95, name = "vents_middle_door", switchSidePlanes = true, },
         { distance = 100, },
     }
 }
