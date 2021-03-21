982fbbf69fdb727028f7efe8f4a900376bc1ab0b
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2021-03-21 20:47:03 +0300

    ending1 level wip

--- config/levels/ending1.lua
+++ config/levels/ending1.lua
@@ -7,8 +7,8 @@ return {
     fogColor = {25, 10, 0},
     fogDistance = 60,
     playerRotationMode = "sinusoid",
-    playerRotationSpeed = 0.3,
-    playerRotationChangeSpeed = 0.01,
+    playerRotationSpeed = -0.3,
+    playerRotationChangeSpeed = 0.012,
 
     sidePlanesCount = 80,
     sidePlanesRandomBrightness = false,
@@ -24,6 +24,19 @@ return {
             textures = {
                 "levels/meat/side_planes/flesh2",
             },
+        },
+        {
+            textures = {
+                "levels/meat/side_planes/flesh1",
+                "levels/meat/side_planes/flesh2",
+            },
+        },
+        {
+            textures = {
+                "levels/vents/decorative1",
+                "levels/vents/decorative2",
+                "levels/vents/decorative3",
+            }
         },
     },
 
@@ -43,6 +56,39 @@ return {
                 { texture = "levels/meat/obstacles/stone_obstacle5", },
             },
         },
+        meat_half_wall = {
+            planes = {
+                { texture = "levels/meat/obstacles/meat_decorative_corner_things2", position = {0, 0, 1}, rotation = 180, decorative = true },
+                { texture = "levels/meat/obstacles/meat_obstacle_half3", position = {0, 0, 0} },
+                { texture = "levels/meat/obstacles/meat_obstacle_half3_2", position = {0, 0, -0.2}, decorative1 = true},
+                { texture = "levels/meat/obstacles/meat_obstacle_half2", position = {0, 0, -2} },
+                { texture = "levels/meat/obstacles/meat_obstacle_half", position = {0, 0, -4} },
+                { texture = "levels/meat/obstacles/meat_obstacle_half_2", position = {0, 0, -4.1}, decorative = true },
+            },
+        },
+        meat_wall = {
+            planes = {
+                { texture = "levels/meat/obstacles/meat_wall", breakable = true }
+            },
+        },
+        meat_long_middle_thing = {
+            planes = {
+                { texture = "levels/meat/obstacles/long_meat_middle_thing" },
+                { texture = "levels/meat/obstacles/long_meat_middle_thing2", position = {0, 0, -0.25}, decorative = true },
+            },
+        },
+
+        vents_middle_long_stick = {
+            planes = {
+                { texture = "levels/vents/long_stick", position = {0, 0, 0}, rotation = 180 },
+            }
+        },
+        vents_middle_door = {
+            planes = {
+                { texture = "levels/vents/middle_door_frame" },
+                { texture = "levels/vents/middle_door", position = {0, 0, -0.5}, velocity = {-8, 0, 0}, moveDelay = 1, },
+            },
+        },
     },
 
     planes = {
@@ -51,6 +97,16 @@ return {
         { distance = 20, name = "stone_long_obstacle", rotation = 90 },
         { distance = 20, name = "stone_long_obstacle", rotation = 270 },
         { distance = 50, name = "stone_breakable_wall", switchSidePlanes = true },
-        { distance = 1500, },
+        { distance = 50, name = "meat_half_wall" },
+        { distance = 30, name = "meat_half_wall", rotation = 180 },
+        { distance = 6, name = "meat_wall", switchSidePlanes = true },
+        { distance = 25, name = "vents_middle_long_stick", rotation = 90 },
+        { distance = 20, name = "meat_half_wall" },
+        { distance = 10, name = "meat_long_middle_thing", rotation = 90},
+        { distance = 10, name = "meat_half_wall", rotation = 90},
+        { distance = 30, name = "meat_wall" },
+
+        { distance = 30, name = "vents_middle_door", switchSidePlanes = true, },
+        { distance = 100, },
     }
 }
