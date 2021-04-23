c35cb548e2cbfe1314f9a0d13450b56bc9b90208
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2021-04-23 18:00:21 +0300

    add daily challenge screen

--- ui/screens/MainMenuScreen.lua
+++ ui/screens/MainMenuScreen.lua
@@ -16,6 +16,7 @@ function MainMenuScreen:initialize()
     self.buttons = {
         { label = "main_menu_btn_play_story", handler = "buttonHandlerPlayStory" },
         { label = "main_menu_btn_play_endless_mode", handler = "buttonHandlerPlayEndlessMode" },
+        { label = "main_menu_btn_play_daily_challenge", handler = "buttonHandlerPlayDailyChallenge" },
         { label = "btn_settings", handler = "buttonHandlerSettings" },
         { label = "main_menu_btn_credits", handler = "buttonHandlerCredits"},
         {},
@@ -73,6 +74,10 @@ end
 
 function MainMenuScreen:buttonHandlerPlayEndlessMode()
     self.screenManager:transition("GameScreen", "endless")
+end
+
+function MainMenuScreen:buttonHandlerPlayDailyChallenge()
+    self.screenManager:transition("DailyChallengeScreen")
 end
 
 function MainMenuScreen:buttonHandlerExit()
