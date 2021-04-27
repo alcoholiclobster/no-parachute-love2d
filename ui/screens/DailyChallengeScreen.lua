4ee71efb8f79481fd475a0c2e4916314f9c0a10a
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2021-04-27 14:56:00 +0300

    daily challenge leaderboards

--- ui/screens/DailyChallengeScreen.lua
+++ ui/screens/DailyChallengeScreen.lua
@@ -4,6 +4,7 @@ local widgets = require("ui.widgets")
 local GameManager = require("core.GameManager")
 local lz = require("utils.language").localize
 local Steam = require("luasteam")
+local LeaderboardView = require("ui.LeaderboardView")
 
 local DailyChallengeScreen = class("DailyChallengeScreen", Screen)
 
@@ -11,33 +12,65 @@ local startDay = 18739
 
 function DailyChallengeScreen:initialize()
     local secondsInDay = 60 * 60 * 24
-    local dayNumber = math.floor(os.time() / secondsInDay)
+    local dayNumber = math.floor(os.time() / secondsInDay) + 1
     local secondsCurrentDay = os.time() - dayNumber * secondsInDay
-    print(math.floor(secondsCurrentDay / (60 * 60)))
 
+    local previousDaySeed = (dayNumber - 1) + 1337
     self.seed = dayNumber + 1337
 
     GameEnv.endlessForceSeed = self.seed
     local endlessLevel = require("config.levels.endless")
     self.gameManager = GameManager:new(endlessLevel, self, true, self.seed)
 
-    self.entries = {}
+    self.globalLeaderboard = LeaderboardView:new({
+        name = "daily_"..tostring(self.seed),
+        type = "Global",
+        title = "Today Global",
+        limit = 15,
+    })
+    self.globalLeaderboard.x = 0.05
+    self.globalLeaderboard.y = 0.2
+    self.globalLeaderboard.width = 0.25
+    self.globalLeaderboard.height = 0.6
 
-    Steam.userStats.findOrCreateLeaderboard("daily_"..tostring(self.seed), "Descending", "Numeric", function (data)
-        Steam.userStats.uploadLeaderboardScore(data.steamLeaderboard, "ForceUpdate", math.random(1, 10), "hihi", function (data)
-            print("uploaded ", data.success, data.globalRankNew)
-        end)
+    self.friendsLeaderboard = LeaderboardView:new({
+        name = "daily_"..tostring(self.seed),
+        type = "Friends",
+        title = "Today Friends",
+        limit = 15,
+    })
+    self.friendsLeaderboard.x = self.globalLeaderboard.x + self.globalLeaderboard.width + 0.05
+    self.friendsLeaderboard.y = 0.2
+    self.friendsLeaderboard.width = 0.25
+    self.friendsLeaderboard.height = 0.6
 
-        Steam.userStats.downloadLeaderboardEntries(data.steamLeaderboard, "Global", 1, 10, function (items)
-            for _, item in ipairs(items) do
-                table.insert(self.entries, {
-                    rank = item.globalRank,
-                    name = Steam.friends.getFriendPersonaName(item.steamIDUser),
-                    score = item.score,
-                })
-            end
-        end)
-    end)
+    self.yesterdayLeaderboard = LeaderboardView:new({
+        name = "daily_"..tostring(previousDaySeed),
+        type = "Global",
+        title = "Yesterday Global",
+        limit = 20,
+    })
+    self.yesterdayLeaderboard.x = self.friendsLeaderboard.x + self.friendsLeaderboard.width + 0.05
+    self.yesterdayLeaderboard.y = 0.2
+    self.yesterdayLeaderboard.width = 0.25
+    self.yesterdayLeaderboard.height = 0.6
+    -- self.entries = {}
+
+    -- Steam.userStats.findOrCreateLeaderboard("daily_"..tostring(self.seed), "Descending", "Numeric", function (data)
+    --     Steam.userStats.uploadLeaderboardScore(data.steamLeaderboard, "ForceUpdate", math.random(1, 10), "hihi", function (data)
+    --         print("uploaded ", data.success, data.globalRankNew)
+    --     end)
+
+    --     Steam.userStats.downloadLeaderboardEntries(data.steamLeaderboard, "Global", 1, 10, function (items)
+    --         for _, item in ipairs(items) do
+    --             table.insert(self.entries, {
+    --                 rank = item.globalRank,
+    --                 name = Steam.friends.getFriendPersonaName(item.steamIDUser),
+    --                 score = item.score,
+    --             })
+    --         end
+    --     end)
+    -- end)
 end
 
 function DailyChallengeScreen:update(deltaTime)
@@ -49,19 +82,26 @@ function DailyChallengeScreen:draw()
 
     self.gameManager:draw()
 
+    love.graphics.setColor(0, 0, 0, 0.6)
+    love.graphics.rectangle("fill", 0, 0, screenWidth, screenHeight)
+
     local secondsInDay = 60 * 60 * 24
     local dayNumber = math.floor(os.time() / secondsInDay)
     local secondsLeft = secondsInDay - (os.time() - dayNumber * secondsInDay)
     local timeLeft = string.format("%.2d:%.2d:%.2d", secondsLeft/(60*60), secondsLeft/60%60, secondsLeft%60)
     love.graphics.setColor(1, 1, 1)
-    widgets.label("Day: "..tostring(dayNumber - startDay)..". Time left: "..timeLeft, screenWidth * 0.1, screenHeight * 0.1, screenWidth * 0.8, screenHeight * 0.05, false, "left")
+    widgets.label("Day: "..tostring(dayNumber - startDay)..". Time left: "..timeLeft, screenWidth * 0.05, screenHeight * 0.07, screenWidth * 0.8, screenHeight * 0.05, false, "left")
 
-    local h = screenHeight * 0.04
-    local y = screenHeight * 0.1 + screenHeight * 0.08
-    for i, item in ipairs(self.entries) do
-        widgets.label(item.rank..". "..item.name.." - "..item.score, screenWidth * 0.1, y, screenWidth * 0.8, h, false, "left")
-        y = y + h * 1.5
-    end
+    -- local h = screenHeight * 0.04
+    -- local y = screenHeight * 0.1 + screenHeight * 0.08
+    -- for i, item in ipairs(self.entries) do
+    --     widgets.label(item.rank..". "..item.name.." - "..item.score, screenWidth * 0.1, y, screenWidth * 0.8, h, false, "left")
+    --     y = y + h * 1.5
+    -- end
+
+    self.globalLeaderboard:draw()
+    self.friendsLeaderboard:draw()
+    self.yesterdayLeaderboard:draw()
 
     -- Back to menu screen button
     if widgets.button(lz("btn_back"), screenWidth * 0.08, screenHeight - screenHeight * 0.1, screenWidth * (0.5 - 0.08 * 2), screenHeight * 0.05) then
